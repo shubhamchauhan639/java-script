d1274e43a0f29bb1af6c0b5f02d5429c38fad02f
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2025-08-11 21:18:29 +0530

    heap and stack

---
+++ memory.js
@@ -0,0 +1,14 @@
+let myName = "Shubhamsinghchauhan"
+ let anotherName = myName
+ anotherName = "shubham"
+console.log(myName);
+console.log(anotherName);
+let userOne = {
+    email: "shubhamchauhan.com",
+    phoneNO : "345678"
+    
+}
+let userTwo = userOne
+userTwo.email = "google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
