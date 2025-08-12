16fe66799a9eb4476d9ccbaf47b0810f197547b5
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2025-08-13 00:14:20 +0530

    rm:files

--- 02datatype.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-let name = "shubham"
-let age = 20
-let isloggedIn = false
-console.log(typeof "shiuhu");
--- nume.js
+++
@@ -1,6 +0,0 @@
-let score = "h"
-console.log( typeof (score));
-let changeIntoNumber = Number(score)
-console.log( typeof (changeIntoNumber));
-let changeIntoBoll = Boolean(score)
-console.log( changeIntoBoll);
