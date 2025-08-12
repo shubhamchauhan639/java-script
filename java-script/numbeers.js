e33d48937476a3f4baffb1f3078cdbd516530df2
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2025-08-12 20:30:14 +0530

    Added number operations in java-script folder

--- java-script/numbeers.js
+++ java-script/numbeers.js
@@ -11,9 +11,8 @@ console.log(otherNumber.toPrecision(6));
 //**************************************maths****************************** 
 console.log(Math.random())
 console.log((Math.random()*10)+1)
-
+console.log(Math.floor(Math.random()*10)+1);
 const min = 10
 const max = 20
 console.log(Math.floor(Math.random() * (max - min + 1))+ min);
-
 
