40ee10190c312b84dca2afa1623bd9dfd940711c
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2025-08-15 23:39:45 +0530

    loopss

--- java-script/loop.js
+++ java-script/loop.js
@@ -1,7 +1,17 @@
-let obj = { harry: 98, rohan: 70, aakash: 73 };
+// let obj = { harry: 98, rohan: 70, aakash: 73 };
 
-let keys = Object.keys(obj);
-for (let i = 0; i < keys.length; i++) {
-  console.log(keys[i] + " got " + obj[keys[i]]);
-}
+// let keys = Object.keys(obj);
+// for (let i = 0; i < keys.length; i++) {
+//   console.log(keys[i] + " got " + obj[keys[i]]);
+// }
 // for of loop
+let correctNumber = 7;
+let guess;
+
+while (guess !== correctNumber) {
+  guess = Number(prompt("Enter the number: "));
+  if (guess !== correctNumber) {
+    console.log("Try again");
+  }
+}
+console.log("Correct number entered!");
