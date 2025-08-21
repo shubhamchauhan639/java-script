eb3d3c0f9f09d9cbce77f8758ff2cef44b5c2a83
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2025-08-21 14:11:54 +0530

    object 2

---
+++ java-script/objects2.js
@@ -0,0 +1,41 @@
+const tinderUser = {}
+
+tinderUser.userName = "Shubham",
+tinderUser.userID = "546",
+tinderUser.userIsLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser ={
+    email : "shubh.gmail.com",
+    fullName :{
+        firstName: "Shubham",
+        lastName: "chauhan"
+    }
+}
+
+console.log(regularUser.fullName.firstName);
+
+const obj = {1: "one", 2: "two", 3: "three"};
+const obj2 = {4: "four", 5: "five", 6: "six"};
+
+const obj3 = {...obj, ...obj2} ;
+console.log(obj3);
+
+const users = [
+    {
+        id : 1,
+        email: "jsn@gmail.com",
+    },
+    {
+        id : 1,
+        email: "jsn@gmail.com",
+    },
+    {
+        id : 1,
+        email: "jsn@gmail.com",
+    }
+]
+users[1].email
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
