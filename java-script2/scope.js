61ade9a2d74797ba7a3cb2e2d7401ef5f4a3d3e7
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2025-08-22 15:38:01 +0530

    scope in func

--- java-script2/scope.js
+++ java-script2/scope.js
@@ -9,3 +9,17 @@ console.log(userName);
 two();
 }
 one();
+
+
+// ********************************************************example **************************************************************************************************
+function addone(num){
+    return num + 1;
+
+}
+console.log(addone(5));
+
+const addTwo = function(num){
+    return num + 2;
+    
+}
+console.log(addTwo(5));
