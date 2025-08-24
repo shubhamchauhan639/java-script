e4b00429505ece2dbcd9fe252a2b0fd4ae41c425
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2025-08-24 22:42:53 +0530

    filter

---
+++ java-script2/highorder.js
@@ -0,0 +1,11 @@
+// const coding = ['html', 'css', 'js', 'react', 'nodejs'];
+// const values=coding.forEach( (item)=>{
+// console.log(item);
+
+// })
+// console.log(values);
+
+const mynumbers = [2, 3, 4, 5, 6,7,8,9,10];
+const newnum=mynumbers.filter( (num) => {
+   return  num>4 })
+console.log(newnum);
