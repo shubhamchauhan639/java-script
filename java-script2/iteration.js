5bb626759a27d63fe077d0d2c62ed89d5c39ca88
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2025-08-24 17:31:16 +0530

    loopsss

---
+++ java-script2/iteration.js
@@ -0,0 +1,12 @@
+for (let i = 0; i <= 20; i++) {
+    
+    console.log(`Table of  ${i} `);
+    
+
+    for (let j = 0; j < 10 ; j++) {
+        console.log(i + `*` + j + `=` + i*j);
+        
+        
+        
+    }
+}
