6628ca487544286609ac28e21742fb4ef209dbaf
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2025-08-25 13:08:20 +0530

    foreach

--- java-script2/highorder.js
+++ java-script2/highorder.js
@@ -6,6 +6,14 @@
 // console.log(values);
 
 const mynumbers = [2, 3, 4, 5, 6,7,8,9,10];
-const newnum=mynumbers.filter( (num) => {
-   return  num>4 })
-console.log(newnum);
+// const newnum=mynumbers.filter( (num) => {
+//    return  num>4 })
+
+
+const newNum = []
+mynumbers.forEach((num)=>{
+    if(num>4){
+        newNum.push(num);
+    }
+})
+console.log(newNum);
