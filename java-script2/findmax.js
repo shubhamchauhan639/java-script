ec852ca9847b5cadb8f99b7f8547110eda8569dd
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2025-08-30 22:07:19 +0530

    find the second largest value

---
+++ java-script2/findmax.js
@@ -0,0 +1,18 @@
+function secondLargest(arr) {
+let firstLargest = -Infinity;
+let secondLargest = -Infinity;
+
+for (let i = 0; i< arr.length; i++) {
+if (arr[i] > firstLargest){
+    secondLargest = firstLargest;
+    firstLargest = arr[i];
+}
+else if (arr[i] > secondLargest && arr[i] != firstLargest) {
+    secondLargest = arr[i];
+}
+}
+return secondLargest;
+}
+let arr = [2,12,5,10,0,3,4];
+let result = secondLargest(arr);
+console.log(result);
