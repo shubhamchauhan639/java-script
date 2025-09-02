cdee5c5eb8c34761f18a064d4642e7f695f70477
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2025-09-02 21:09:12 +0530

    call back function

---
+++ java-script/callfunction.js
@@ -0,0 +1,10 @@
+function square (n) {
+ return n*n
+    }
+function sumOfSquare (a,b){
+    const value1 = square(a)
+    const value2 = square(b)
+    return value1 + value2
+}
+console.log(sumOfSquare(1,2))
+
