0707834abf3c332ec8918dc98ef83b83ab69d7e9
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2025-09-10 15:25:57 +0530

    assync and set timout

--- java-script/01var.js
+++ java-script/01var.js
@@ -1,16 +1,20 @@
-const accountId = 1232
-let accountEmail = "shuabhamchauna1906@google.com"
-var accountPassw = "1234"
-accountCity = "delhi"
-let accountState;
+// const accountId = 1232
+// let accountEmail = "shuabhamchauna1906@google.com"
+// var accountPassw = "1234"
+// accountCity = "delhi"
+// let accountState;
 
 
 
 
-//accountId = 2
+// //accountId = 2
 
-accountEmail="jsak@.com"
-accountPassw="kvf"
-accountCity ="jaipur"
-console.log(accountId);
-console.table([accountId,accountEmail,accountPassw,accountCity,accountState]);
+// accountEmail="jsak@.com"
+// accountPassw="kvf"
+// accountCity ="jaipur"
+// console.log(accountId);
+// console.table([accountId,accountEmail,accountPassw,accountCity,accountState]);
+
+let a = 198 ;
+let b =69;
+console.log(a+b)
