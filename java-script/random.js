3a9248d04ef411440bdfa5147327ef14646bef3d
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2025-09-12 20:57:28 +0530

    otp

--- java-script/random.js
+++ java-script/random.js
@@ -1,10 +1,10 @@
 function generateOTP(length = 6) {
   let otp = '';
   for (let i = 0; i < length; i++) {
-    otp += Math.floor(Math.random() * 10); // random digit 0-9
+    otp += Math.floor(Math.random() * 10); 
   }
   return otp;
 }
 
-console.log(generateOTP());   // Example: "407913"
-console.log(generateOTP(4));  // Example: "5821"
+console.log(generateOTP());   
+console.log(generateOTP(4));  
